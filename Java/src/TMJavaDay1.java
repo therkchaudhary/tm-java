80e88e63628523f428e07baf755c5db1ae6f51c0
therkchaudhary <rkchaudhary9354@gmail.com>
2026-07-07 20:15:19 +0530

    First commit

---
+++ Java/src/TMJavaDay1.java
@@ -0,0 +1,8 @@
+public class TMJavaDay1 {
+        public static void main(String[] args) {
+            System.out.println("Hello, TalentxMinds!");
+            System.out.println("My name is Rahul Chaudhary"); // Replace with your name
+            System.out.println("AI Policy Rule: Always use AI responsibly, ethically, and respect privacy and security.");
+        }
+
+}
