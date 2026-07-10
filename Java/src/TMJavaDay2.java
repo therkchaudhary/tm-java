538b00e5f68aafa0f2864a5e4914e9f1cd89498c
therkchaudhary <rkchaudhary9354@gmail.com>
2026-07-10 10:10:34 +0530

    Day 2 Assesment

---
+++ Java/src/TMJavaDay2.java
@@ -0,0 +1,34 @@
+public class TMJavaDay2 {
+    public static void main(String[] args) {
+        String name = "Rahul Chaudhary";
+        int age = 20;
+        String college = "IIMT College of Engineering";
+        String course = "B.Tech CSE";
+        String city = "Ghaziabad";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("College: " + college);
+        System.out.println("Course: " + course);
+        System.out.println("City: " + city);
+
+
+
+//***********************************************************************
+// Write a program to print different datatype.
+        int num = 100;
+        float decimal = 12.5f;
+        double pi = 3.14159;
+        char grade = 'A';
+        boolean result = true;
+        String StdName = "Rahul";
+
+        System.out.println("Integer: " + num);
+        System.out.println("Float: " + decimal);
+        System.out.println("Double: " + pi);
+        System.out.println("Character: " + grade);
+        System.out.println("Boolean: " + result);
+        System.out.println("String: " + name);
+
+    }
+}
